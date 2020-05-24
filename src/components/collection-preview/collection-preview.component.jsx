ebbb225e8e7921be9c8de18bd707b5d0b7c18401
Ashleigh Waldek <ashleighwaldek@gmail.com>
2020-05-24 17:30:29 -0400

    new shop data; added cart util to group cart items

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -9,9 +9,9 @@ const CollectionPreview = ({ title, items }) => (
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
       {items
-        .filter((i, ind) => ind < 4)
-        .map(({ id, ...otherItemProps }) => (
-          <CollectionItem key={id} {...otherItemProps} />
+        .filter((item, ind) => ind < 4)
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
         ))}
     </div>
   </div>
