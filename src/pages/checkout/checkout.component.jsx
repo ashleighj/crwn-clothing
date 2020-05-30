56e0c27162f50d98f0c5f04d74064549ba90c262
Ashleigh Waldek <ashleighwaldek@gmail.com>
2020-05-30 19:02:16 -0400

    add/remove item functionality in checkout item component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -19,7 +19,7 @@ const CheckoutPage = ({ cartItems, cartTotal }) => (
                 <span>Quantity</span>
             </div>
             <div className="header-block">
-                <span>Proce</span>
+                <span>Price</span>
             </div>
             <div className="header-block">
                 <span>Remove</span>
